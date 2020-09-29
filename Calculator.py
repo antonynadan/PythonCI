334dce8bfba8d4ebea64e5d57c54fa81617a70d1
AntonyNadan <antonynadan10@gmail.com>
2020-09-29 22:30:56 +0530

    Added a file

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,8 @@
 def add(first_term, second_term):
-    return first_term + second_term
+    result = first_term + second_term
+    return result
 
 
 def subtract(first_term, second_term):
-    return first_term - second_term
+    result = first_term - second_term
+    return result
